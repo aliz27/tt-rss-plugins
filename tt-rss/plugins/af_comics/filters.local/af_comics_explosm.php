a5a2b83a4f4983f6dc6906b34fd4500288e5dd14
Daniel Ahlberg <aliz@tamperd.net>
2018-10-04 17:52:23 +0200

    Skip links to episodes and support failed plugin

---
+++ tt-rss/plugins/af_comics/filters.local/af_comics_explosm.php
@@ -0,0 +1,33 @@
+<?php
+class Af_Comics_Explosm extends Af_ComicFilter {
+
+	function supported() {
+		return array("Cyanide and Happiness");
+	}
+
+	function process(&$article) {
+
+		if (strpos($article["link"], "explosm.net/comics") !== FALSE) {
+
+				$doc = new DOMDocument();
+
+				if (@$doc->loadHTML(fetch_file_contents($article["link"]))) {
+					$xpath = new DOMXPath($doc);
+					$basenode = $xpath->query('(//img[@id="main-comic"])')->item(0);
+
+					$checkEpisode = $xpath->query('(//a[contains(@href,"episode")])')->item(0);
+					if (!$checkEpisode) {
+						if ($basenode) {
+							$article["content"] = $doc->saveHTML($basenode);
+						} else {
+							$article["failed"] = true;
+						}
+					}
+				}
+
+			return true;
+		}
+
+		return false;
+	}
+}
