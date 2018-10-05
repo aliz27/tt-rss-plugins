7d943263947c285fed5e96c438b9d6bdade13e39
Daniel Ahlberg <aliz@tamperd.net>
2018-10-04 18:56:29 -0600

    add af_inline_image

---
+++ tt-rss/plugins.local/af_inline/filters/af_inline_image.php
@@ -0,0 +1,22 @@
+<?php
+class Af_Inline_VReddIt extends Af_InlineFilter {
+	function supported() {
+		return array("images");
+	}
+
+	function process(&$article, &$entry, &$doc, &$found, &$inline, $debug) {
+		if ((preg_match("/\.(jpg|jpeg|gif|png)$/i", $entry->getAttribute("href")) ||
+			preg_match("/i.reddituploads.com/i", $entry->getAttribute("href")) ||
+			preg_match("/i.redditmedia.com/i", $entry->getAttribute("href"))) {
+
+			_debug("Handling as a picture", $debug);
+
+			$this->handle_as_image($doc, $entry, $entry->getAttribute("href"));
+
+			$found = true;
+			return true;
+		}
+
+		return false;
+	}
+}
