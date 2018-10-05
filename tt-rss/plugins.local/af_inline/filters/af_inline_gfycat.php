b89734091c24e78a96b782fa8c45a4be930219b4
Daniel Ahlberg <aliz@tamperd.net>
2018-10-04 20:21:43 -0600

    add af_inline_gfycat

---
+++ tt-rss/plugins.local/af_inline/filters/af_inline_gfycat.php
@@ -0,0 +1,22 @@
+<?php
+class Af_Inline_Gfycat extends Af_InlineFilter {
+	function supported() {
+		return array("Gfycat");
+	}
+
+	function process(&$article, &$entry, &$doc, &$found, &$inline, $debug) {
+		if (preg_match("/gfycat.com\/([A-Z0-9]+)$/i", $entry->getAttribute("href"), $matches)) {
+			_debug("Found gfycat.com ". $entry->getAttribute("href"), $debug);
+			$req = "https://gfycat.com/cajax/get/".$matches[1];
+			$resp = json_decode($inline->apiCall($req), true);
+
+			if ($resp["gfyItem"]) {
+				$inline->handle_as_video($doc, $entry, $resp["gfyItem"]["webmUrl"], $resp["gfyItem"]["posterUrl"], $debug);
+				$found = "video";
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
