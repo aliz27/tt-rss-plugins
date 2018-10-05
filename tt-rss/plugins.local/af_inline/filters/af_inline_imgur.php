d3bd779105b5236feb0fffa7dc0e76d9b0c8c012
Daniel Ahlberg <aliz@tamperd.net>
2018-10-04 19:41:21 -0600

    add af_inline_imgur

--- tt-rss/plugins.local/af_inline/filters/af_inline_imgur.php
+++ tt-rss/plugins.local/af_inline/filters/af_inline_imgur.php
@@ -12,7 +12,7 @@ class Af_Inline_Imgur extends Af_InlineFilter {
 			if (preg_match("/imgur\.com\/([A-Z0-9]+)[\/]?$/i", $url, $matches)) {
 				_debug("(imgur) link ".$url, $debug);
 
-				$resp = $this->imgurLookup($matches[1], "image", $debug);
+				$resp = $inline->imgurLookup($matches[1], "image", $debug);
 
 				$url = $resp["data"]["link"];
 			}
@@ -20,7 +20,7 @@ class Af_Inline_Imgur extends Af_InlineFilter {
 			if (preg_match("/imgur\.com\/r\/([A-Z0-9]+)\/([A-Z0-9]+)[\/]?$/i", $url, $matches)) {
 				_debug("(imgur) subreddit link ".$url, $debug);
 
-				$resp = $this->imgurLookup("/r/".$matches[1]."/".$matches[2], "gallery", $debug);
+				$resp = $inline->imgurLookup("/r/".$matches[1]."/".$matches[2], "gallery", $debug);
 
 				$url = $resp["data"]["link"];
 			}
@@ -34,15 +34,15 @@ class Af_Inline_Imgur extends Af_InlineFilter {
 			if (preg_match("/([A-Z0-9]+)\.(gif|gifv|mp4|webm)$/i", $url, $matches) ) {
 				_debug("(imgur) animated?: ".$url, $debug);
 
-				$resp = $this->imgurLookup($matches[1],"image",$debug);
+				$resp = $inline->imgurLookup($matches[1],"image",$debug);
 
 				if ($resp["data"]["mp4"]) {
 					_debug("(imgur) gif(v) mp4: ".$resp["data"]["mp4"], $debug);
-					$this->handle_as_video($doc, $entry, $resp["data"]["mp4"], $resp["data"]["link"], $debug);
+					$inline->handle_as_video($doc, $entry, $resp["data"]["mp4"], $resp["data"]["link"], $debug);
 					$found = "video";
 				} else {
 					_debug("(imgur) gif(v) gif: ".$resp["data"]["link"], $debug);
-					$this->handle_as_image($doc, $entry, $resp["data"]["link"]);
+					$inline->handle_as_image($doc, $entry, $resp["data"]["link"]);
 					$found = true;
 				}
 			}
@@ -52,7 +52,7 @@ class Af_Inline_Imgur extends Af_InlineFilter {
 
 				if ($matches[1] == "a") { $lookup = "album"; } else { $lookup = "gallery"; }
 
-				$resp = $this->imgurLookup($matches[2],$lookup,$debug);
+				$resp = $inline->imgurLookup($matches[2],$lookup,$debug);
 
 				if ($resp["data"]["images"]) { $images = $resp["data"]["images"]; } else { $images[0]["link"] = $resp["data"]["link"]; } 
 
@@ -62,22 +62,22 @@ class Af_Inline_Imgur extends Af_InlineFilter {
 					if (preg_match("/([A-Z0-9]+)\.gif[v]?$/i", $image["link"], $matches) ) {
 						_debug("(imgur) gif(v): ".$url, $debug);
 
-						$resp_gif = $this->imgurLookup($matches[1],"image",$debug);
+						$resp_gif = $inline->imgurLookup($matches[1],"image",$debug);
 
 						if ($resp_gif["data"]["mp4"]) {
 							_debug("(imgur) gif(v) mp4: ".$resp_gif["data"]["mp4"], $debug);
-							$this->handle_as_video($doc, $entry, $resp_gif["data"]["mp4"], $resp_gif["data"]["link"], $debug);
+							$inline->handle_as_video($doc, $entry, $resp_gif["data"]["mp4"], $resp_gif["data"]["link"], $debug);
 							$found = "video";
 						} else {
 							_debug("(imgur) gif(v) gif: ".$resp_gif["data"]["link"], $debug);
-							$this->handle_as_image($doc, $entry, $resp_gif["data"]["link"]);
+							$inline->handle_as_image($doc, $entry, $resp_gif["data"]["link"]);
 							$found = true;
 						}
 					}
 
 					if (preg_match("/\.(jpg|jpeg|png)$/i", $image["link"])) {
 						_debug("(imgur) album/gallery image ".$image["link"], $debug);
-						$this->handle_as_image($doc, $entry, $image["link"]);
+						$inline->handle_as_image($doc, $entry, $image["link"]);
 						$found = true;
 					}
 				}
