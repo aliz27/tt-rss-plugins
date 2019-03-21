ff17ce103829490bc241814d6f77c44595e8e336
aliz <aliz@tamperd.net>
2019-03-21 21:11:17 +0100

    fix gfycat

--- tt-rss/plugins.local/af_inline/filters/af_inline_gfycat.php
+++ tt-rss/plugins.local/af_inline/filters/af_inline_gfycat.php
@@ -4,14 +4,13 @@ class Af_Inline_Gfycat extends Af_InlineFilter {
 		return array("Gfycat");
 	}
 
-	function process(&$article, &$entry, &$doc, &$found, &$inline, $debug) {
+	function process(&$article, &$entry, &$doc, &$found, &$inline, $debug = false) {
 		if (preg_match("/gfycat.com\/([A-Z0-9]+)$/i", $entry->getAttribute("href"), $matches)) {
 			_debug("Found gfycat.com ". $entry->getAttribute("href"), $debug);
 //			$req = "https://gfycat.com/cajax/get/".$matches[1];
-			$resp = json_decode($inline->gfycatLookup($matches[1]), true);
-
+			$resp = $inline->gfycatLookup($matches[1], $debug);
 			if ($resp["gfyItem"]) {
-				$inline->handle_as_video($doc, $entry, $resp["gfyItem"]["webmUrl"], $resp["gfyItem"]["posterUrl"], $debug);
+				$inline->handle_as_video($doc, $entry, $resp["gfyItem"]["mp4url"], $resp["gfyItem"]["posterUrl"], $debug);
 				$found = "video";
 				return true;
 			}
