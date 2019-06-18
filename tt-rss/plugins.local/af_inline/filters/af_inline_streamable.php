fc2936c5a4bdd6724a7989d3bd3f996f822c33af
aliz <aliz@tamperd.net>
2019-06-18 22:38:12 +0200

    embed streamable

--- tt-rss/plugins.local/af_inline/filters/af_inline_streamable.php
+++ tt-rss/plugins.local/af_inline/filters/af_inline_streamable.php
@@ -6,13 +6,25 @@ class Af_Inline_Streamable extends Af_InlineFilter {
 
 	function process(&$article, &$entry, &$doc, &$found, &$inline, $debug) {
 		if (preg_match("/streamable.com\/([A-Z0-9]+)$/i", $entry->getAttribute("href"), $matches)) {
-                        _debug("Found streamable.com ". $entry->getAttribute("href"), $debug);
-                        $req = "https://api.streamable.com/videos/".$matches[1];
-                        $resp = json_decode(fetch_file_contents($req, false, $context), true);
 
-                        $inline->handle_as_video($doc, $entry, $resp["files"]["mp4"]["url"], $resp["files"]["thumbnail_url"], $debug);
+			$vid_id = $matches[1];
+
+			_debug("Handling as streamable: $vid_id", $debug);
+
+			$iframe = $doc->createElement("iframe");
+			$iframe->setAttribute("class", "streamable-embed");
+			$iframe->setAttribute("width", "1280");
+			$iframe->setAttribute("height", "720");
+			$iframe->setAttribute("src", "https://www.youtube.com/embed/$vid_id");
+			$iframe->setAttribute("allowfullscreen", "1");
+			$iframe->setAttribute("frameborder", "0");
+			$iframe->setAttribute("scrolling", "no");
+
+			$br = $doc->createElement('br');
+			$entry->parentNode->insertBefore($iframe, $entry);
+			$entry->parentNode->insertBefore($br, $entry);
 			array_push($article["tags"], "video");
-                        $found = "true";
+			$found = true;
 			return true;
 		}
 		return false;
