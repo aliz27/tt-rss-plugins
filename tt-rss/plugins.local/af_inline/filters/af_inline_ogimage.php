84fd6f90ea85426f4849d6b1eaa7c1f4288cbaf0
aliz <aliz@tamperd.net>
2019-06-20 22:50:18 +0200

    Handle og:image

---
+++ tt-rss/plugins.local/af_inline/filters/af_inline_ogimage.php
@@ -0,0 +1,35 @@
+<?php
+class Af_Inline_OgImage extends Af_InlineFilter {
+	function supported() {
+		return array("og:image");
+	}
+
+	function process(&$article, &$entry, &$doc, &$found, &$inline, $debug) {
+		if (preg_match("/imgflip.com\//i", $entry->getAttribute("href")) 
+/*||
+			preg_match("/i.reddituploads.com/i", $entry->getAttribute("href")) ||
+			preg_match("/i.redditmedia.com/i", $entry->getAttribute("href"))) */ {
+			_debug("Looking for og:image", $debug);
+			$page = fetch_file_contents($entry->getAttribute("href"));
+
+                        $pageDoc = new DOMDocument();
+
+                        if (@$pageDoc->loadHTML($page)) {
+                                $pageXPath = new DOMXPath($pageDoc);
+                                $images = $pageXPath->query("//meta[@property='og:image']");
+
+                                foreach ($images as $image) {
+					_debug("Found og:image: ".$image->getAttribute("content"), $debug);
+					$inline->handle_as_image($doc, $entry, $image->getAttribute("content"));
+					array_push($article["tags"], "image");
+					$found = true;
+					return true;
+                                        }
+                                }
+                        }
+
+		}
+
+		return false;
+	}
+}
